0d76d4a5540495d8158bce21bc5bd1dd128468b6
manishr7 <143963938+manishr7@users.noreply.github.com>
2024-08-16 23:27:49 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,16 @@ import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
+import cors from 'cors';
+const app = express();
+app.use(cors());
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
-const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
